2f56c710606edb4fa76240f0a3e307601f1cb2f5
thewlyn <nguyenhang251104@gmail.com>
2024-05-19 23:42:26 +0700

    2

---
+++ src/views/ChangePassword.vue
@@ -0,0 +1,246 @@
+<script setup>
+// import { ref } from 'vue'
+// import axios from 'axios'
+// import { useRouter } from 'vue-router'
+
+// const currentPassword = ref('')
+// const newPassword = ref('')
+// const confirmPassword = ref('')
+// const lastname = ref('')
+
+// const router = useRouter()
+// const isSuccess = ref(false)
+// import Notification from './Notification.vue'
+
+// const checkUsername = () => {
+// 	const regex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{2,}$/
+// 	isUsernameValid.value = regex.test(username.value)
+// }
+// const showSuccessNotification = () => {
+// 	isSuccess.value = true
+// 	setTimeout(() => {
+// 		isSuccess.value = false
+// 	}, 5000)
+// 	console.log('User registered successfully!')
+// }
+
+// const changePassword = async () => {
+// 	try {
+// 		const response = await axios.post(
+// 			,
+// 			{
+
+// 			}
+// 		)
+// 		console.log(response.data)
+// 		isSuccess.value = true
+// 		setTimeout(() => {
+// 			router.push('/login')
+// 		}, 5000)
+// 	} catch (error) {
+// 		console.error(error)
+// 	}
+// }
+</script>
+<template>
+	<div class="all">
+		<div class="form-wrapper">
+			<main class="form-side">
+				<form class="my-form" @submit.prevent="changePassword">
+					<div class="form-welcome-row">
+						<h1>Đổi mật khẩu</h1>
+					</div>
+					<div class="text-field">
+						<input
+							type="text"
+							id="currentPassword"
+							name="currentPassword"
+							placeholder="Nhập mật khẩu hiện tại"
+							v-model="currentPassword"
+							required
+						/>
+					</div>
+					<div class="text-field">
+						<input
+							id="newPassword"
+							type="text"
+							name="newPassword"
+							placeholder="Nhập mật khẩu mới"
+							v-model="newPassword"
+							required
+						/>
+					</div>
+					<div class="text-field">
+						<input
+							id="confirmPasword"
+							type="text"
+							name="confirmPasword"
+							placeholder="Nhập lại mật khẩu mới"
+							v-model="confirmPasword"
+							required
+						/>
+					</div>
+
+					<button type="submit" class="my-form__button">Đổi mật khẩu</button>
+				</form>
+			</main>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+}
+.form-welcome-row h1 {
+	text-align: center;
+	font-size: 2.5rem;
+	line-height: 2rem;
+	font-weight: bold;
+	margin-top: 3rem;
+	margin-bottom: 1rem;
+}
+.form-wrapper {
+	position: relative;
+	display: grid;
+	/* grid-template-columns: 6fr 5fr; */
+	margin: 0 auto;
+	height: 50rem;
+}
+
+.form-side {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	padding: 0 2rem;
+}
+.my-form {
+	display: flex;
+	flex-direction: column;
+	justify-content: start;
+	position: relative;
+	gap: 1.25rem;
+	max-width: 23rem;
+	width: 45rem;
+}
+
+.my-form__button {
+	font-weight: bold;
+	background-color: black;
+	color: white;
+	white-space: nowrap;
+	border: 2px solid black;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	margin-top: 1.5rem;
+	line-height: 2.75rem;
+	outline: none;
+	font-size: 1.125rem;
+	text-decoration: none;
+	cursor: pointer;
+	width: 100%;
+	border-radius: 0.5rem;
+	box-shadow: 0 5px 10px rgba(0, 0, 0, 0.15);
+	transition: all 0.3s ease;
+}
+.my-form__button:hover {
+	background-color: white;
+	color: black;
+}
+.my-form:invalid .my-form__button {
+	pointer-events: none;
+}
+
+.my-form__actions a:hover {
+	text-decoration: underline;
+}
+
+.my-form__row {
+	display: flex;
+	gap: 0.5rem;
+	justify-content: space-between;
+}
+
+.text-field {
+	display: flex;
+	flex-direction: column;
+	gap: 0.5rem;
+}
+
+.text-field input {
+	color: var(--secondary);
+	max-width: 100%;
+	width: 100%;
+	border: 1px solid var(--border);
+	min-height: 2.75rem;
+	letter-spacing: 0.03rem;
+	outline: none;
+	background-color: var(--input-bg);
+	transition: 0.25s;
+	border-radius: 0.5rem;
+	text-indent: 1.25rem;
+}
+.text-field label {
+	color: black;
+}
+
+.text-field input:user-invalid {
+	background-color: var(--white);
+	color: var(--error);
+	border: 1px solid var(--error);
+}
+
+.error-message {
+	display: none;
+}
+
+.text-field input:user-invalid ~ .error-message {
+	display: block;
+	margin-top: 0.25rem;
+	font-size: 0.8rem;
+	color: var(--error);
+}
+
+.text-field input:focus {
+	border: 1px solid var(--text);
+	box-shadow: rgba(99, 99, 99, 0.2) 0px 1px 4px 0px;
+}
+.notification {
+	position: absolute;
+	width: max-content;
+	left: 0;
+	right: 0;
+	bottom: 1.5rem;
+	margin-left: auto;
+	margin-right: auto;
+	border-radius: 0.375rem;
+	background-color: black;
+	color: white;
+	box-shadow: 0 1px 10px rgba(0, 0, 0, 0.3);
+	transform: translateY(1.875rem);
+	opacity: 0;
+	visibility: hidden;
+	animation: fade-in 3s linear;
+}
+
+@keyframes fade-in {
+	5% {
+		opacity: 1;
+		visibility: visible;
+		transform: translateY(0);
+	}
+	95% {
+		opacity: 1;
+		transform: translateY(0);
+	}
+}
+
+@keyframes progress {
+	to {
+		transform: scaleX(1);
+	}
+}
+</style>
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,61 +1,90 @@
 <template>
-	<div class="card-client">
-		<div class="user-picture">
-			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-				<path
-					d="M224 256c70.7 0 128-57.31 128-128s-57.3-128-128-128C153.3 0 96 57.31 96 128S153.3 256 224 256zM274.7 304H173.3C77.61 304 0 381.6 0 477.3c0 19.14 15.52 34.67 34.66 34.67h378.7C432.5 512 448 496.5 448 477.3C448 381.6 370.4 304 274.7 304z"
-				></path>
-			</svg>
+	<div class="container">
+		<div class="background-overlay"></div>
+		<div class="card-client">
+			<div class="user-picture">
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+					<path
+						d="M224 256c70.7 0 128-57.31 128-128s-57.3-128-128-128C153.3 0 96 57.31 96 128S153.3 256 224 256zM274.7 304H173.3C77.61 304 0 381.6 0 477.3c0 19.14 15.52 34.67 34.66 34.67h378.7C432.5 512 448 496.5 448 477.3C448 381.6 370.4 304 274.7 304z"
+					></path>
+				</svg>
+			</div>
+			<div class="infomation">
+				<div class="column-small">
+					<p class="info-text">Họ và tên:</p>
+					<p class="info-text">Tên đăng nhập:</p>
+					<p class="info-text">Ngày sinh:</p>
+					<p class="info-text">Giới tính:</p>
+					<p class="info-text">Email:</p>
+				</div>
+				<div class="column-large">
+					<p class="info-text">Nguyễn Văn A</p>
+					<p class="info-text">abcd9@</p>
+					<p class="info-text">01/01/2000</p>
+					<p class="info-text">Nam</p>
+					<p class="info-text">nguyenvana@gmail.com</p>
+				</div>
+			</div>
+			<div class="button">
+				<button class="card__btn">Thêm bạn</button>
+				<button class="card__btn">Nhắn tin</button>
+			</div>
 		</div>
-		<p class="infomation">
-			<p class="info-text">First name: {{ user.first_name }}</p>
-			<p class="info-text">Last name: {{ user.last_name }}</p>
-			<p class="info-text">Username: {{ user.username }}</p>
-			<!-- <p class="info-text">Birtday: {{ user.birthday }}</p>
-			<p class="info-text">Gender: {{ user.gender }}</p> -->
-			<p class="info-text">Email: {{ user.email }}</p>
-		</p>
 	</div>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue'
-import axios from 'axios'
+//   import { ref, onMounted } from 'vue'
+//   import axios from 'axios'
 
-const user = ref(null)
-const user_id =  ref() // Thay thế bằng ID người dùng thực tế hoặc nhận từ route hoặc props
+//   const user = ref(null)
+//   const user_id = ref() // Thay thế bằng ID người dùng thực tế hoặc nhận từ route hoặc props
 
-const fetchUserProfile = async () => {
-	try {
-		const response = await axios.get(
-			`http://127.0.0.1:8000/api/v1/user/profile/${user_id}`
-		)
-		user.value = response.data // Đảm bảo rằng response.data có cấu trúc { first_name, last_name, email }
-	} catch (error) {
-		console.error('Failed to fetch user profile:', error)
-	}
-}
+//   const fetchUserProfile = async () => {
+// 	try {
+// 	  const response = await axios.get(
+// 		`http://127.0.0.1:8000/api/v1/user/profile/${user_id}`
+// 	  )
+// 	  user.value = response.data // Đảm bảo rằng response.data có cấu trúc { first_name, last_name, email }
+// 	} catch (error) {
+// 	  console.error('Không thể lấy thông tin hồ sơ người dùng:', error)
+// 	}
+//   }
 
-onMounted(() => {
-	fetchUserProfile()
-})
+//   onMounted(() => {
+// 	fetchUserProfile()
+//   })
 </script>
 
 <style>
+.container {
+	position: relative;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	height: 100vh;
+}
+
+.background-overlay {
+	position: fixed;
+	width: 100%;
+	height: 100%;
+	background-color: rgba(0, 0, 0, 0.5);
+	z-index: 1;
+}
+
 .card-client {
-	background: #555;
-	width: 13rem;
-	padding-top: 25px;
-	padding-bottom: 25px;
-	padding-left: 20px;
-	padding-right: 20px;
-	border: 4px solid #fff;
-	box-shadow: 0 6px 10px #212121;
+	position: relative;
+	background: black;
+	width: 30rem;
+	padding: 25px 20px;
+	border: 5px solid black;
+	box-shadow: 0 6px 10px #2c2b2b;
 	border-radius: 10px;
-	text-align: center;
 	color: #fff;
 	font-family: 'Poppins', sans-serif;
 	transition: all 0.3s ease;
+	z-index: 2; /* Ensure it's above the overlay */
 }
 
 .card-client:hover {
@@ -81,15 +110,56 @@ onMounted(() => {
 }
 
 .infomation {
-	margin: 0;
-	margin-top: 20px;
-	font-weight: 600;
-	font-size: 18px;
+	display: flex;
+	justify-content: space-between;
+	margin-top: 35px;
+}
+.column-small {
+	flex: 1;
 }
 
-.infomation span {
-	display: block;
-	font-weight: 200;
-	font-size: 16px;
+.column-large {
+	flex: 2;
+}
+
+.info-text {
+	font-family:
+		system-ui,
+		-apple-system,
+		BlinkMacSystemFont,
+		'Segoe UI',
+		Roboto,
+		Oxygen,
+		Ubuntu,
+		Cantarell,
+		'Open Sans',
+		'Helvetica Neue',
+		sans-serif;
+	font-size: 18px;
+	font-weight: bolder;
+	margin-bottom: 10px;
+}
+.button {
+	margin-left: 65px;
+	margin-top: 30px;
+}
+.card__btn {
+	margin-left: 30px;
+	width: 100px;
+	height: 37px;
+	border: 2px solid black;
+	border-radius: 4px;
+	font-weight: 700;
+	font-size: 15px;
+	color: black;
+	background: white;
+	transition: all 0.3s;
+}
+
+.card__btn:hover {
+	background: black;
+	color: white;
+	border: 2px solid white;
+	border-radius: 4px;
 }
 </style>
